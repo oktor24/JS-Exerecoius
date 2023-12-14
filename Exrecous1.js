64269df69e7374d76979f1aa1c29ab99d4004e2c
oktor24 <110783171+oktor24@users.noreply.github.com>
2023-12-14 09:51:30 +0330

    Create Exrecous1.js

---
+++ Exrecous1.js
@@ -0,0 +1,80 @@
+const myNodelist = document.querySelectorAll("p");
+for (let i = 0; i < myNodelist.length; i++) {
+  myNodelist[i].style.color = "red";
+}
+
+const myNodelist = document.querySelectorAll("p");
+for (let i = 0; i < myNodelist.length; i++) {
+  myNodelist[i].style.color = "da";
+
+
+  const myNodelist = document.querySelectorAll("p");
+for (let i = 0; i < myNodelist.length; i++) {
+  myNodelist[i].style.color = "ewr";
+}
+
+  if (event.altKey) {
+  alert("The ALT key was pressed!");
+} else {
+  alert("The ALT key was NOT pressed!");
+}
+
+  if (event.metaKey) {
+  alert("The META key was pressed!");
+} else {
+  alert("The META key was NOT pressed!");
+}
+
+
+  if (event.altKey) {
+  alert("The ALT key was pressed!");
+} else {
+  alert("The ALT key was NOT pressed!");
+}
+
+document.getElementById("myDIV").addEventListener("transitionend", myFunction);
+
+function myFunction(event) {
+  this.innerHTML = "Transition lasted: " + event.elapsedTime + " seconds";
+}
+
+
+const myNodelist = document.querySelectorAll("p");
+for (let i = 0; i < myNodelist.length; i++) {
+  myNodelist[i].style.color = "red";
+}
+
+const myNodelist = document.querySelectorAll("p");
+for (let i = 0; i < myNodelist.length; i++) {
+  myNodelist[i].style.color = "da";
+
+
+  const myNodelist = document.querySelectorAll("p");
+for (let i = 0; i < myNodelist.length; i++) {
+  myNodelist[i].style.color = "ewr";
+}
+
+  if (event.altKey) {
+  alert("The ALT key was pressed!");
+} else {
+  alert("The ALT key was NOT pressed!");
+}
+
+  if (event.metaKey) {
+  alert("The META key was pressed!");
+} else {
+  alert("The META key was NOT pressed!");
+}
+
+
+  if (event.altKey) {
+  alert("The ALT key was pressed!");
+} else {
+  alert("The ALT key was NOT pressed!");
+}
+
+document.getElementById("myDIV").addEventListener("transitionend", myFunction);
+
+function myFunction(event) {
+  this.innerHTML = "Transition lasted: " + event.elapsedTime + " seconds";
+}
