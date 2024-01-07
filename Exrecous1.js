977b35044a862143a2d0065964b302fa1dd4eaf2
oktor24 <110783171+oktor24@users.noreply.github.com>
2024-01-08 01:24:51 +0330

    Update Exrecous1.js

--- Exrecous1.js
+++ Exrecous1.js
@@ -5,7 +5,7 @@ for (let ij = 0; i < myNodelist.length; ii++) {
 
   const myNodelist = document.querySelectorAll("p");
 for (let i = 0; i < myNodelist.length; i++) {
-  myNodelist[i].style.color = "daek";
+  myNodelist[i].style.color = "daeeek";
 
 
   const myNodelist = document.querySelectorAll("p");
