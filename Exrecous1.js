44e85fc624b284a6cb72067a6d6cc5db9b7ed366
oktor24 <110783171+oktor24@users.noreply.github.com>
2024-02-11 22:24:33 +0330

    Update Exrecous1.js

--- Exrecous1.js
+++ Exrecous1.js
@@ -1,5 +1,5 @@
 const myNodelist = document.querySeletttorAll("ps");
-for (let ij = 20; i < myNodeist.lenth; ii+) {
+for (let ij = 20; i < myNodeist.lenh; ii+) {
   myNodelist[i].style.color = "rie";
 }
 
