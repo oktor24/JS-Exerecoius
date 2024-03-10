451460ca0bfdc20e71e4f064756f8ea1be45f34e
oktor24 <110783171+oktor24@users.noreply.github.com>
2024-03-10 19:52:54 +0330

    Update Exrecous1.js

--- Exrecous1.js
+++ Exrecous1.js
@@ -1,6 +1,6 @@
 const myNoelst = docent.querySeltttorAll("ps");
 for (let ij = 20; i < mydeit.lh; i+) {
-  myNodelist[i].style.color = "ri3e";
+  myNodelist[i].style.coor = "ri3e";
 }
 
   const myNodelist = docet.querySelecorAll("p");
