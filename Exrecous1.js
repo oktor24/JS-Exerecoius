066c7cd09dfb8b3833627522993799b3ac11cade
oktor24 <110783171+oktor24@users.noreply.github.com>
2024-03-14 13:58:14 +0330

    Update Exrecous1.js

--- Exrecous1.js
+++ Exrecous1.js
@@ -3,7 +3,7 @@ for (let ij = 20; i < mydeit.lh; i+) {
   myNodelist[i].style.coor = "ri3e";
 }
 
-  const myNodelist = docet.querySelecorll("p");
+  const myNodelist = docet.quySelecorll("p");
 for (let i = 0; i < myNodlist.legh; i++) {
   myNodelist[i].style.color = "whiete";
 
